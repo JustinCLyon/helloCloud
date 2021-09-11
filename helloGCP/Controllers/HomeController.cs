d43380e2e4059f9222a597f6abded7426f02f384
endle <endle@DESKTOP-L7BK2RB>
2021-09-11 12:17:42 -0400

    Add project files.

---
+++ helloGCP/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using helloGCP.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace helloGCP.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private IWebHostEnvironment Environment;
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(IWebHostEnvironment _environment, ILogger<HomeController> logger)
+        {
+            Environment = _environment;
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            List<FruitViewModel> fruitModel = FruitViewModel.GetFruitModelList(GetFruitList());
+
+            return View("ListFruits", fruitModel);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public fruits GetFruitList()
+        {
+            string rootPath = this.Environment.ContentRootPath;
+            string xmlFilePath = rootPath + "/Data/Fruits.xml";
+
+            fruits fruitList = null;
+
+            using (FileStream fs = new FileStream(xmlFilePath, FileMode.Open))
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(fruits));
+                fruitList = (fruits)serializer.Deserialize(fs);
+                fs.Close();
+            }
+            return fruitList;
+        }
+    }
+}
---
+++ helloGCP/Models/FruitViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace helloGCP.Models
+{
+    public class FruitViewModel
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Fruit")]
+        public string Name { get; set; }
+
+        [Display(Name = "Shape")]
+        public string Shape { get; set; }
+
+        [Display(Name = "Harvest Season")]
+        public String HarvestSeason { get; set; }
+
+        [Display(Name = "Family")]
+        public string Family { get; set; }
+
+        public static List<FruitViewModel> GetFruitModelList(fruits fruitList)
+        {
+            List<FruitViewModel> fruitViewList = new List<FruitViewModel>();
+            int i = 1;
+
+            foreach (fruitsFruit fr in fruitList.Items) 
+            {
+                FruitViewModel currentFruit = new FruitViewModel();
+
+                currentFruit.Id = i;
+                currentFruit.Name = fr.name;
+                currentFruit.Shape = fr.shape;
+                currentFruit.HarvestSeason = fr.harvestseason.ToString();
+                currentFruit.Family = fr.family;
+
+                fruitViewList.Add(currentFruit);
+                i++;
+            }
+            return fruitViewList;
+        }
+    }
+}
